2b932a3656f5bc59ed18f44bfe86e3c1c5c87d59
Pragati <pragati.mane@161099@gmail.com>
2024-05-23 20:26:22 +0530

    Adding array

--- Page1.js
+++ Page1.js
@@ -1,3 +1,9 @@
 console.log('Hello!')
 let num = 100
 console.log(`num = ${num} , type =${typeof(num)}`)
+
+function function3()
+{
+    const numbers = [10,20,30,40,50]
+    console.log(numbers)
+}
